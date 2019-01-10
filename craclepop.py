448d3a6add60328787c9f51a2b520c65b59438c3
Richard Casey <richard.casey@locationlabs.com>
2019-01-10 03:21:12 -0800

    initial commit

---
+++ craclepop.py
@@ -0,0 +1,47 @@
+
+def stepdirection(start, end):
+    # determine if we are stepping up or down
+    if start > end:
+        end -= 1
+        step = -1
+        return [start, end, step]
+    else:
+        end += 1
+        step = None
+        return [start, end, step]
+
+
+
+def crackiterable(start=-1, end=100):
+    try:
+        totalcount = 0
+        cracklepopcount = 0
+        snapcount = 0
+        cracklecount = 0
+        for number in range(stepdirection(start, end)):
+            totalcount += 1
+            if number % 3 == 0 and number % 5 == 0:
+                print("{0} - Cracklepop!\n").format(number)
+                cracklepopcount += 1
+            elif number % 3 == 0 and not number % 5 == 0:
+                print("{0} - Snap!\n").format(number)
+                snapcount += 1
+            elif number % 5 == 0 and not number % 3 == 0:
+                print("{0} - Crackle!\n").format(number)
+                cracklecount += 1
+            else:
+                print("{0}\n").format(number)
+        print("""
+        In the range from {0} to {1}, there were:
+        {2} Snaps, {3} Crackles, and {4} Cracklepops""").format(
+        start, end, snapcount, cracklecount, cracklepopcount)
+
+    except TypeError:
+        print("You've not provided an integer for start and end length")
+        raise
+    except:
+        print("Unexpected error!")
+        raise
+
+
+crackiterable()
