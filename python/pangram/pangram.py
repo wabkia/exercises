8d1230fdc05b38701c34c0ec04b9ea77053d34d1
gruvdev <richard@trickyserv.com>
2019-01-11 11:34:32 -0800

    fucking windows

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,13 +1,13 @@
 def is_pangram(sentence):
 
 
-	alphaset = frozenset("abcdefghijklmnopqrstuvxyz")
-	try:
-		while alphaset.issuperset(frozenset(lower(sentence))):
-			return True
-		else:
-		    return False
+    alphaset = frozenset("abcdefghijklmnopqrstuvxyz")
+    try:
+        while alphaset.issuperset(frozenset(lower(sentence))):
+            return True
+        else:
+            return False
     except TypeError:
-    	print("hey now, this isn't a good input")
-    	raise
+        print("hey now, this isn't a good input")
+        raise
     
