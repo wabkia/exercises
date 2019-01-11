693fc3dd00082af201485427a3560033632bebc3
gruvdev <richard@trickyserv.com>
2019-01-11 12:30:01 -0800

    adding better error catching to two_fer.py

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,4 +1,6 @@
 def two_fer(name="you"):
 
     while isinstance(name, str):
-    	return "One for {}, one for me.".format(name)
+    	    return "One for {}, one for me.".format(name)
+    else:
+    	raise TypeError("You didn't provide a string for a name")
