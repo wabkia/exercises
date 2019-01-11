3788eb308708b4a4d2dbb89e2a77d989c44e4418
gruvdev <richard@trickyserv.com>
2019-01-11 15:27:06 -0800

    armstrong_numbers refactored

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -2,14 +2,14 @@ def is_armstrong(number):
     # get a length of the number then check Armstrong condition
     # to return true or false
     while isinstance(number, int):
-        numberlength = len(str(abs(number)))
-        numbersplit = number.split('')
+        numberlength = int(len(str(abs(number))))
+        numbersplit = [int(str(number)[i:i+1]) for i in range(0, len(str(number)), 1)]
         armstrongtotal = 0
-    try:
-        for n in numbersplit:
-            armstrongtotal += n * numberlength 
-        while number == armstrongtotal:
-            return
-    except:
-        print("somethign went wrong, like you didn't give an integer to the function")
-        raise
+
+        try:
+            for n in numbersplit:
+                armstrongtotal += n ** numberlength
+            return number == armstrongtotal
+        except:
+            print("something went wrong, like you didn't give an integer to the function")
+            raise
