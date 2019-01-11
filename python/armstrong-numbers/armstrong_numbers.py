ddcfd34da5b6b8d58eddc9ea0cabbf62fcf4c23d
gruvdev <richard@trickyserv.com>
2019-01-11 13:38:02 -0800

    add integer check before setting variables in armstrong_numbers

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,9 +1,10 @@
 def is_armstrong(number):
     # get a length of the number then check Armstrong condition
     # to return true or false
-    numberlength = len(str(abs(number)))
-    numbersplit = number.split('')
-    armstrongtotal = 0
+    while number isinstance(number, int):
+        numberlength = len(str(abs(number)))
+        numbersplit = number.split('')
+        armstrongtotal = 0
     try:
         for n in numbersplit:
             armstrongtotal += n * numberlength 
